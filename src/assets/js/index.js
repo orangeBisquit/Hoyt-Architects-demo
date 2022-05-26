25ecba625005d10c0b16ec7c214b33c2c496d7e2
orangeBisquit <62883132+orangeBisquit@users.noreply.github.com>
2022-05-26 12:04:43 +0600

    sarasota added

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -28,6 +28,7 @@ class ProjectApp {
 		const layout = document.querySelector('[data-layout]');
 		const logo = document.querySelector('._v1');
 		const work = document.querySelector('._v2');
+		const sarasota = document.querySelector('._v3');
 		const studio = document.querySelector('._v4');
 		const contact = document.querySelector('._v5');
 		const lab = document.querySelector('._v6');
@@ -57,6 +58,11 @@ class ProjectApp {
 			layout.dataset.layout = 'v5';
 			body.classList = '';
 			body.classList.add('orange');
+		});
+		sarasota.addEventListener('click', () => {
+			layout.dataset.layout = 'v6';
+			body.classList = '';
+			body.classList.add('purple');
 		});
 	};
 }
